dd28243cf077635dc18e601e566c8992ff3ac98c
Viviane de Lima <delimaviviane@yahoo.com.br>
2022-04-05 21:41:28 -0300

    first commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,40 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.0.2"
+  constraints = ">= 2.26.0"
+  hashes = [
+    "h1:QPPDLJTDEVDkBU1HOuXYtKnFULKoOOJeDnUb1GmS8bE=",
+    "zh:4d7dcf1318f23202a33ac1e7dbed692ec80d86813fc50149bd46863e6c9584eb",
+    "zh:64171745d17f10d9cc60ae77443b5439a84fb3cc69d164f3ffe527c5f271e6da",
+    "zh:7bb19cbb5702b19ae010af3f8b8c949b183748e1859165e7268d00ea62f4e460",
+    "zh:7f6e634e9d9c5b43a4ff36f8797f51f00fa13df36361a80f3ca7482349279963",
+    "zh:84f64143e66f627b035fef9345271059d3ce51fea2eb4c52188647ef01124a82",
+    "zh:89ce9fc1a4ec40a0904b68a24fe1e598a4a89d42fc91772c89de57a88c862890",
+    "zh:96b610037db94ae23e7f73ba3296164d761319b7943fc7443f63029e3db7cc9b",
+    "zh:a216690bf0d33b43580ba80088dcc4cb3b39fa0c8eb0e9d221d9f02ca39df41b",
+    "zh:b4f39b1b37d9b29230e36c00e825f279337b2bb9e53b3d3b1ee0544782794606",
+    "zh:b686d9615d2627e1631e6cc07b1a61d9865ea43db8042ea5365fcc9ac5b1f465",
+    "zh:bc96d3bc3d122f81fea271bba5104ba32860d5e84f03da19751e5e8cb883ebef",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version = "3.1.1"
+  hashes = [
+    "h1:1J3nqAREzuaLE7x98LEELCCaMV6BRiawHSg9MmFvfQo=",
+    "zh:063466f41f1d9fd0dd93722840c1314f046d8760b1812fa67c34de0afcba5597",
+    "zh:08c058e367de6debdad35fc24d97131c7cf75103baec8279aba3506a08b53faf",
+    "zh:73ce6dff935150d6ddc6ac4a10071e02647d10175c173cfe5dca81f3d13d8afe",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8fdd792a626413502e68c195f2097352bdc6a0df694f7df350ed784741eb587e",
+    "zh:976bbaf268cb497400fd5b3c774d218f3933271864345f18deebe4dcbfcd6afa",
+    "zh:b21b78ca581f98f4cdb7a366b03ae9db23a73dfa7df12c533d7c19b68e9e72e5",
+    "zh:b7fc0c1615dbdb1d6fd4abb9c7dc7da286631f7ca2299fb9cd4664258ccfbff4",
+    "zh:d1efc942b2c44345e0c29bc976594cb7278c38cfb8897b344669eafbc3cddf46",
+    "zh:e356c245b3cd9d4789bab010893566acace682d7db877e52d40fc4ca34a50924",
+    "zh:ea98802ba92fcfa8cf12cbce2e9e7ebe999afbf8ed47fa45fc847a098d89468b",
+    "zh:eff8872458806499889f6927b5d954560f3d74bf20b6043409edf94d26cd906f",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,161 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers{
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "rg-aulainfra" {
+  name     = "aulainfracloudterra"
+  location = "centralus"
+}
+resource "azurerm_virtual_network" "vnet-aulainfra" {
+  name                = "vnet"
+  location            = azurerm_resource_group.rg-aulainfra.location
+  resource_group_name = azurerm_resource_group.rg-aulainfra.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "Production"
+    turma = "FS04"
+    faculdade = "Impacta"
+    aluno = "Viviane de Lima"
+    Professor = "João Henrique"
+  }
+}
+
+
+resource "azurerm_subnet" "sub-aulainfra" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg-aulainfra.name
+  virtual_network_name = azurerm_virtual_network.vnet-aulainfra.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "ip-aulainfra" {
+  name                    = "publicip"
+  location                = azurerm_resource_group.rg-aulainfra.location
+  resource_group_name     = azurerm_resource_group.rg-aulainfra.name
+  allocation_method       = "Static"
+
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg-aulainfra" {
+  name                = "nsg"
+  location            = azurerm_resource_group.rg-aulainfra.location
+  resource_group_name = azurerm_resource_group.rg-aulainfra.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "80"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_interface" "nic-aulainfra" {
+  name                = "nic"
+  location            = azurerm_resource_group.rg-aulainfra.location
+  resource_group_name = azurerm_resource_group.rg-aulainfra.name
+
+  ip_configuration {
+    name                          = "nic-ip"
+    subnet_id                     = azurerm_subnet.sub-aulainfra.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.ip-aulainfra.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic-nsg-aulainfra" {
+  network_interface_id      = azurerm_network_interface.nic-aulainfra.id
+  network_security_group_id = azurerm_network_security_group.nsg-aulainfra.id
+}
+
+resource "azurerm_storage_account" "sa-aulainfra" {
+  name                     = "saaulainfra"
+  resource_group_name      = azurerm_resource_group.rg-aulainfra.name
+  location                 = azurerm_resource_group.rg-aulainfra.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm-aulainfra" {
+  name                = "vm"
+  resource_group_name = azurerm_resource_group.rg-aulainfra.name
+  location            = azurerm_resource_group.rg-aulainfra.location
+  size                = "Standard_D2ads_v5"
+  network_interface_ids = [
+    azurerm_network_interface.nic-aulainfra.id,
+  ]
+
+   admin_username      = "adminuser"
+   admin_password      = "Password1234!" 
+   disable_password_authentication = false
+   
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    name = "mydisk"
+    caching = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.sa-aulainfra.primary_blob_endpoint
+  }
+
+}
+
+data "azurerm_public_ip" "ip-aulainfra-data"{
+    name = azurerm_public_ip.ip-aulainfra.name
+    resource_group_name = azurerm_resource_group.rg-aulainfra.name
+}
+
+resource "null_resource" "install-webserver" {
+
+  connection {
+    type = "ssh"
+    host = data.azurerm_public_ip.ip-aulainfra-data.ip_address
+    user = "adminuser"
+    password = "Password1234!"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y apache2"
+    ]
+  }
+
+    depends_on = [
+      azurerm_linux_virtual_machine.vm-aulainfra
+    ]
+}
